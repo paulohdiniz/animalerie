0bd0f682abfc373474e8f772ab40607002f3ca9c
Paulo <ph.diniz@usp.br>
2022-11-19 00:43:51 +0100

    equipement and animals static ok

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.farm, name='farm'),
+    path('animals', views.animals, name = 'animals'),
     path('animal/<str:id_animal>/', views.animaldetail, name='animal_detail'),
+    path('equipements', views.equipements, name = 'equipements'),
+    path('equipements/<str:id_equip>/', views.equipementdetail, name='equipement_detail'),
     #path('animal/<str:id_animal>/?<str:message>', views.animal_detail, name='animal_detail_mes'),
 
 
--- blog/views.py
+++ blog/views.py
@@ -13,8 +13,23 @@ def farm(request):
 
     return render(request, 'blog/farm.html', {'animal': animal, 'equipement': equipement})
 
+def animals(request):
+    animal = Animal.objects.filter()
+    return render(request, 'blog/animals.html', {'animal': animal})
+
 def animaldetail(request, id_animal):
     animal = get_object_or_404(Animal, id_animal=id_animal)
     lieu = animal.lieu
     return render(request, 'blog/animal_detail.html', {'animal': animal})
+
+def equipements(request):
+    equipement = Equipement.objects.filter()
+    return render(request, 'blog/equipements.html', {'equipement': equipement})
+
+def equipementdetail(request, id_equip):
+    equipement = get_object_or_404(Equipement, id_equip=id_equip)
+    animal = Animal.objects.filter(lieu = equipement.id_equip)
+    return render(request, 'blog/equipement_detail.html', {'animal': animal, "equipement": equipement})
+
+
 
